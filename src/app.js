41420e6993e0ec90e925817f55c3558b9c39c4b4
dodsonm <matthew.dodson.dc@gmail.com>
2017-09-28 03:03:27 -0400

    React form to dynamically add items to list

--- src/app.js
+++ src/app.js
@@ -8,16 +8,57 @@ class App extends React.Component {
     }
   }
 
+  addItem(e) {
+    e.preventDefault();
+    const {buyItems} = this.state;
+    const newItem = this.newItem.value;
+
+    this.setState({
+      buyItems: [...this.state.buyItems, newItem]
+    });
+
+    this.addForm.reset();
+  }
+
   render() {
     const {buyItems} = this.state;
     return (
       <main>
-        <h1>Shopping List</h1>
-        {
-          buyItems.map(item => {
-            return <p key={item}>{item}</p>
-          })
-        }
+        <header>
+          <h1>Shopping List</h1>
+          <form ref={input => this.addForm = input} className="form-inline" onSubmit={(e) => {this.addItem(e)}}>
+            <div className="form-group">
+              <label className="form-label sr-only" htmlFor="newItemInput">Add New Item</label>
+              <input ref={input => this.newItem = input} type="text" placeholder="Add..." className="form-control" id="newItemInput" />
+            </div>
+            <button type="submit" className="btn btn-primary">Add</button>
+          </form>
+        </header>
+        <div className="content">
+          <table className="table">
+            <caption>Things</caption>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                buyItems.map(item => {
+                  return (
+                    <tr key={item}>
+                      <td scope="row">1</td>
+                      <td>{item}</td>
+                      <td>Button</td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </table>
+        </div>
       </main>
     )
   }
